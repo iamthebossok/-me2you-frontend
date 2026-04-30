7eb08694a74b06601b23313e68ebabeb2c4206b4
iamthebossok <rajshaan1@icloud.com>
2026-04-30 21:05:06 +0100

    Add homepage for TheScripto app

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+export default function Home() {
+
+  return (
+
+    <main style={{ padding: 40, textAlign: "center" }}>
+
+      <h1>TheScripto 🚀</h1>
+
+      <p>Your app is now LIVE</p>
+
+    </main>
+
+  );
+
+}
